ae45c7aa5a6355d000dd0127b03d2138f8eb52d9
Mohammad Mahdi Mohammadi <mmm.6831.allah@gmail.com>
2024-10-31 15:26:49 +0330

    Fixed minor bug

--- Services/HandleDbBackup.cs
+++ Services/HandleDbBackup.cs
@@ -13,7 +13,7 @@ public class HandleDbBackup
     {
         logger?.LogInformation("Backup requested");
         
-        await using Stream stream = System.IO.File.OpenRead("./" + dbname);
+        await using Stream stream = System.IO.File.OpenRead("./" + dbname+".json");
         var message = await _bot.SendDocumentAsync(adminId, document: InputFile.FromStream(stream, dbname+".json"),
             caption: "Backup of " + dbname + "\nCreated at " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss" + "\nCurrent post count: " + _db.AsQueryable().Count()), parseMode: ParseMode.Html);
 
